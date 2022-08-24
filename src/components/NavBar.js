2b1b6d4942cfbc9e4fe86e61b2093199347d06c0
Melisa Primon <melprim@gmail.com>
2022-08-24 08:16:36 -0300

    agregando NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../assets/LOGO CORTINAS.png'
+
+const NavBar = () => {
+  return (
+    <div className='contenedor'>
+        <img className='imgLogo' src = {logo} alt='imagenlogo'/>
+        <ul>
+            <li><a href=" ">Home</a></li>
+            <li><a href=" ">Tipos de Cortinas</a></li>
+            <li><a href=" ">Como medir e Instalar</a></li>
+            <li><a href=" ">Contacto</a></li>
+            <li><a className='comprar' href=" ">COMPRÁ AHORA!</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
